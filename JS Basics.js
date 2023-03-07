200793cd5bd78bae09ba38c21caa9bb3e8f858da
MasterPin <you@example.com>
2023-03-07 10:43:05 +0100

    Navbar and Showcase view added

---
+++ JS Basics.js
@@ -0,0 +1,27 @@
+//This is my first JS code!
+console.log("Hello world");
+
+//this is a comments
+/*
+this is a multiline comment
+*/
+
+//Primitive Data Types:
+let object = null;
+let undefined = undefined;
+let boolean = true;
+let number = 1;
+let String = "string";
+
+//keyword let/var(dont use var) is dynamic and 
+//can change during runtime
+
+//Objects:
+let person ={
+    name: "Aleks",
+    age: "25",
+    favFood:"Döner",
+    jobDesc:"Programmer",
+}
+
+console.log("Following person is stores in a object with the following variables" + person);
